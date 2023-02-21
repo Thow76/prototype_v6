38cf2544654ba848e5d47231b34b48d61f260062
Andrew Thow <thow1976@hotmail.co.uk>
2023-02-21 20:15:30 +0000

    main_page_sign_in_sign_up_screen

---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:prototype_v5/services/authentication/widgets/sign_in_sign_up_form_widget.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:
+          // Safe area prevents safe guards widgets to go beyond device edges
+          SafeArea(
+        //===========//
+        // to dismiss keyword on tap outside use listener
+        child: Listener(
+            onPointerDown: (PointerDownEvent event) =>
+                FocusManager.instance.primaryFocus?.unfocus(),
+            //===========//
+            child: const SignInUpForm()),
+      ),
+    );
+  }
+}
